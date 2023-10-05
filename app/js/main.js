a1d711cb17319d5eb2d672e9394ce62036533041
milevsky76 <milevsky76@yahoo.com>
2023-10-05 16:43:26 +0300

    add section specialists

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,31 @@ $(document).ready(function () {
       autoplaySpeed: 5000
     });
   }
+
+  specialistTablinks();
 });
+
+// Табы раздела "Кому подойдёт?"
+function specialistTablinks() {
+  let specTablinks = document.querySelectorAll('.specialists__tablink');
+
+  for (let specTablink of specTablinks) {
+    specTablink.addEventListener('mouseover', (evt) => {
+      let tablink = document.querySelector('.specialists__tablink.active');
+
+      if (tablink !== evt.target) {
+        tablink.classList.remove('active');
+        evt.target.classList.add('active');
+      }
+    });
+
+    specTablink.addEventListener('focus', (evt) => {
+      let tablink = document.querySelector('.specialists__tablink.active');
+
+      if (tablink !== evt.target) {
+        tablink.classList.remove('active');
+        evt.target.classList.add('active');
+      }
+    });
+  }
+}
